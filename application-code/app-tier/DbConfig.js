b394949a670d4709fb1345c6e2e07d8409a228d2
Sunny Sangle <sunnysangle681@gmail.com>
2024-11-16 13:56:37 +0530

    Initial commit: Uploading AWS 3-tier architecture project

---
+++ application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'my-3-tier-project-database.chm60kq6oaq1.ap-south-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'sunny12345',
+    DB_DATABASE : 'webappdb'
+});
